1b29b7201a399e28ce816290010fe6152b544135
Hector Alejandro Franco Berrios <halejandrobf@gmail.com>
2023-02-07 14:23:10 -0600

    semana 4

---
+++ semana 4/consumeapi/src/components/countries/countries.vue
@@ -0,0 +1,27 @@
+<template>
+    {{ data.map((d) => d.region ) }}
+</template>
+
+<script>
+import request from './request';
+export default({
+    data(){
+        return{
+            data:[],
+        }
+    },
+    methods:{
+        async getAllCountries(){
+            const response = await fetch(request.getAllCountries);
+            this.data = await response.json();
+        },
+        async getbyRegion(){
+            const response = await fetch(request.getbyRegion('americas'));
+            this.data = await response.json()
+        }
+    },
+    created(){
+        this.getbyRegion();
+    }
+});
+</script>
---
+++ semana 4/consumeapi/src/components/countries/filters.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>Filtros por region</h3>
+        <div v-for="region in allRegions" :key  = "region.id">
+            <input type="Radio" name="SelectRegion" :value="region"> {{ region }}
+        </div>        
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            allRegions:
+            ['all', 'Europe', 'Asia', 'Africa', 'Americas', 'Oceania', 'Antartic']
+        }
+    }
+}
+</script>
